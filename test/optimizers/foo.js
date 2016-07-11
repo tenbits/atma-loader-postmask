15c011b2f1ccf65ddef62e6cd3ab9e46cff23099
Alex Kit <source.kit@gmail.com>
2016-07-11 19:52:57 +0200

    update (postmask)

--- test/optimizers/foo.js
+++ test/optimizers/foo.js
@@ -1,7 +1,7 @@
-var mask = require('maskjs');
+module.exports = function(mask) {
 
-mask.registerOptimizer('Foo', function(node, next){
-	console.log('YESSS');
-	mask.jmask(node).find('h4').tag('h5');
-	next({ deep: false });
-});
+	mask.registerOptimizer('Foo', function(node, next){
+		mask.jmask(node).find('h4').tag('h5');
+		next({ deep: false });
+	});
+};
